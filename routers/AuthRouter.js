0013c002cb6b28a92efe437d346068a6eb326438
Murat Sahin <yazilimogren1111@gmail.com>
2020-12-13 16:04:48 +0100

    end of backend

--- routers/AuthRouter.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const AuthController = require('../Controllers/AuthController')
-
-const AuthRouter = ('/',AuthController);
-module.exports= AuthRouter;
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
-const express = require('express');
-app =express();
+const express = require("express");
+const app = express();
 
-const router = require('./routers/router')
+require("dotenv").config();
 
-app.use('/api', router);
+const connectDB = require("./models/connectDB");
+const router = require("./routes/router");
 
-app.listen(5001, ()=>{
-    console.log('I am listening to 5001')
+connectDB();
+
+app.use(express.json());
+app.use("/api", router);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`I'm listening on port ${port}`);
 });
-
