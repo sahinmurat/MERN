185c9d2493a8166b6285016358e35f03d063b421
Murat Sahin <yazilimogren1111@gmail.com>
2020-12-14 13:50:25 +0100

    heroku check

--- Controllers/ProfileController.js
+++ Controllers/ProfileController.js
@@ -3,6 +3,7 @@ const User = require("../models/UserModel");
 exports.getProfileInfo = async (req, res) => {
   try {
     const user = await User.findById(req.decodedUser._id).select("-password");
+    console.log(user);
     res.status(200).json(user);
   } catch (err) {
     console.error(err.message);
